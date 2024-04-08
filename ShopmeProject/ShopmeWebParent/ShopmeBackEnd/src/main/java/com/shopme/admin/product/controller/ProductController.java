a75836a364b7775f61360d72b96cf544d0c48d09
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-04-08 12:57:01 +0200

    Products pagination + sort + filter

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/controller/ProductController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/controller/ProductController.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -42,11 +43,41 @@ public class ProductController {
 	
 	// Handler method to list products
 	@GetMapping("/products")
-	public String listAll(Model model) {
-		List<Product> listProducts = productService.listAll();
-		model.addAttribute("listProducts", listProducts);
-		return "products/products";
+	public String listFirstPage(Model model) {
+		return listByPage(1, model, "name", "asc", null);
 	}
+	
+	// Handler method to list by page
+		@GetMapping("/products/page/{pageNum}")
+		public String listByPage(
+				@PathVariable (name = "pageNum") int pageNum,
+				Model model,
+				@RequestParam("sortField") String sortField,
+				@RequestParam("sortDir") String sortDir,
+				@RequestParam(name = "keyword" , required = false) String keyword) {
+			
+			Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword);
+			List<Product> listProducts = page.getContent();
+			
+			long startCount = (pageNum - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
+			long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
+			if(endCount > page.getTotalElements()) {
+				endCount = page.getTotalElements();
+			}
+			String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+			
+			model.addAttribute("currentPage", pageNum);
+			model.addAttribute("totalPages", page.getTotalPages());
+			model.addAttribute("startCount", startCount);
+			model.addAttribute("endCount", endCount);
+			model.addAttribute("totalItems", page.getTotalElements());
+			model.addAttribute("sortField", sortField);
+			model.addAttribute("sortDir", sortDir);
+			model.addAttribute("reverseSortDir", reverseSortDir);
+			model.addAttribute("keyword", keyword);
+			model.addAttribute("listProducts", listProducts);
+			return "products/products";
+		}
 	
 	// Handler method to show product registration form
 	@GetMapping("/products/new")
